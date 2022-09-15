5474d50f8aa6c375b16208d4d03d88f095734804
nverhulp <105401594+nverhulp@users.noreply.github.com>
2022-09-15 14:16:23 -0700

    function to initialize app

--- index.js
+++ index.js
@@ -7,136 +7,148 @@ const { generateMarkdown } = require('./utils/generateMarkdown');
 const questions = () => {
     return inquirer.prompt([
         {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project?',
-        validate: titleInput => {
-            if(titleInput) {
-                return true;
-            } else  {
-                console.log('You must enter a title');
-                return false;
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('You must enter a title');
+                    return false;
+                }
             }
-        }
-    }, 
-    {
-        type: 'input',
-        name: 'about',
-        message: 'Please provide a description of your project',
-        validate: aboutInput => {
-            if(aboutInput) {
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Please provide a description of your project',
+            validate: aboutInput => {
+                if (aboutInput) {
+                    return true;
+                } else {
+                    console.log('You must enter a description');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'Enter installation instructions',
+            validate: installInput => {
+                if (installInput) {
+                    return true;
+                } else {
+                    console.log('You must enter installation instructions');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What is the usage information for your project?',
+            validate: usageInput => {
+                if (usageInput) {
+                    return true;
+                } else {
+                    console.log('You must enter your usage information');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'list',
+            name: 'license',
+            choices: ['MIT', 'ISC', 'GNU LGPL', 'Apache', 'BSD', 'Unlicense', 'None']
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'Enter your contribution guidelines for this project',
+            validate: contributionInput => {
+                if (contributionInput) {
+                    return true;
+                } else {
+                    console.log('You must enter your contribution guidelines');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'testing',
+            message: 'Input testing instructions for the user',
+            validate: testingInput => {
+                if (testingInput) {
+                    return true;
+                } else {
+                    console.log('You must enter testing instructions');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: 'What is your GitHub username?',
+            validate: usernameInput => {
+                if (usernameInput) {
+                    return true;
+                } else {
+                    console.log('You must enter your GitHub username');
+                    return false;
+                }
+            }
+        },
+        type, 'input',
+        name, 'profile',
+        message, 'Enter your GitHub Profile Link',
+        validate, profileInput => {
+            if (profileInput) {
                 return true;
             } else {
-                console.log('You must enter a description');
+                console.log('You must enter your GitHub Profile Link');
                 return false;
             }
-        }
-    }, 
-    {
-        type: 'input',
-        name: 'install',
-        message: 'Enter installation instructions',
-        validate: installInput => {
-            if(installInput) {
+        },
+        type, 'input',
+        name, 'email',
+        message, 'What is your email address?',
+        validate, emailInput => {
+            if (emailInput) {
                 return true;
             } else {
-                console.log('You must enter installation instructions');
+                console.log('You must enter your email address');
                 return false;
             }
         }
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'What is the usage information for your project?',
-        validate: usageInput => {
-            if(usageInput) {
-                return true;
-            } else {
-                console.log('You must enter your usage information');
-                return false;
-            }
-        }
-    },
-    {
-       type: 'list',
-       name: 'license',
-       choices: ['MIT', 'ISC', 'GNU LGPL', 'Apache', 'BSD', 'Unlicense', 'None'] 
-    },
-    {
-        type: 'input',
-        name: 'contribution',
-        message: 'Enter your contribution guidelines for this project',
-        validate: contributionInput => {
-            if(contributionInput) {
-                return true;
-            } else {
-                console.log('You must enter your contribution guidelines');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'testing',
-        message: 'Input testing instructions for the user',
-        validate: testingInput => {
-            if(testingInput) {
-                return true;
-            } else {
-                console.log('You must enter testing instructions');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'username',
-        message: 'What is your GitHub username?',
-        validate: usernameInput => {
-            if(usernameInput) {
-                return true;
-            } else {
-                console.log('You must enter your GitHub username');
-                return false;
-            }
-        }
-    },
-    type, 'input',
-    name, 'profile',
-    message, 'Enter your GitHub Profile Link',
-    validate, profileInput => {
-        if(profileInput) {
-            return true;
-        } else {
-            console.log('You must enter your GitHub Profile Link');
-            return false;
-        }
-    },
-    type, 'input',
-    name, 'email', 
-    message, 'What is your email address?',
-    validate, emailInput => {
-        if(emailInput) {
-            return true;
-        } else {
-            console.log('You must enter your email address');
-            return false;
-        }
-    }
-])
+    ])
+
+        .then(data => {
+            return generateMarkdown(data);
+        })
+        .then(pageMarkdown => {
+            return writeToFile(pageMarkdown);
+        })
+        .catch(err => {
+            console.log(err);
+        })
 }
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, err => {
-        if(err) throw err;
+        if (err) throw err;
         console.log('README has been created')
-})
+    })
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    promptQuestions()
+}
 
 // Function call to initialize app
 init();
